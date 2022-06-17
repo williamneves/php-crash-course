79716b9d4671143484f847d31896f9aee25e8d03
William Neves <drwilliamneves@gmail.com>
2022-06-17 11:21:34 -0400

    trainning

--- _starter_files/01_output.php
+++ _starter_files/01_output.php
@@ -1,6 +1,6 @@
 <?php
 /* -------- Output & Comments ------- */
-
+echo 'hello hello maria';
 /*
   A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
 */
